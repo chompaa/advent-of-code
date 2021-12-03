09c67b9e048dd35df14dd5bc141a895221fd421b
chompaa <antony.m.3012@gmail.com>
2021-12-03 19:01:01 +0000

    minor adjustment to d3 solution

--- day_3/solver.py
+++ day_3/solver.py
@@ -1,6 +1,6 @@
 from copy import deepcopy
 
-with open("input.txt", "r") as f:
+with open("input2.txt", "r") as f:
     lines = [line.strip() for line in f.readlines()]
 
 # part 1
@@ -23,8 +23,7 @@ for i in range(len(bit_sum)):
         break
 
     oxy_crit = 0 if oxy_sum[i] >= len(oxy) / 2 else 1
-    oxy_queue = [line for line in oxy if line[i] == oxy_crit]
-    oxy = [line for line in oxy if line not in oxy_queue]
+    oxy = list(filter(lambda j: j[i] != oxy_crit, oxy))
     oxy_sum = [sum(bit) for bit in zip(*oxy)]
 
 co2 = deepcopy(lines)
@@ -35,11 +34,10 @@ for i in range(len(bit_sum)):
         break
 
     co2_crit = 0 if co2_sum[i] < len(co2) / 2 else 1
-    co2_queue = [line for line in co2 if line[i] == co2_crit]
-    co2 = [line for line in co2 if line not in co2_queue]
+    co2 = list(filter(lambda j: j[i] != co2_crit, co2))
     co2_sum = [sum(bit) for bit in zip(*co2)]
 
-oxy = int("".join(str(bit) for i in oxy for bit in i), 2)
-co2 = int("".join(str(bit) for i in co2 for bit in i), 2)
+oxy = "".join(str(bit) for i in oxy for bit in i)
+co2 = "".join(str(bit) for i in co2 for bit in i)
 
-print(oxy * co2)
+print(int(oxy, 2) * int(co2, 2))
