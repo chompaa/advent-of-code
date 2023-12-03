ddf4c9cf705df712c2858b73ba090b066c48e541
chompaa <antony.m.3012@gmail.com>
2023-12-02 19:53:34 -0500

    doc strings + import organisation

--- make.py
+++ make.py
@@ -1,16 +1,39 @@
-import sys
 import os.path
 import shutil
+import sys
 
 import requests
 
 
-def get_session_id():
+def get_session_id() -> str:
+    """
+    Returns the session ID from the "session.cookie" file.
+
+    Returns:
+        str: The session ID.
+    """
+
     with open("session.cookie", "r") as f:
         return f.read().strip()
 
 
-def make_input(year, day, path):
+def make_input(year: int, day: int, path: str) -> None:
+    """
+    Fetches the input for the specified year and day from the Advent of Code
+    website and saves it to the specified path.
+
+    Args:
+        year (int): The year.
+        day (int): The day.
+        path (str): The path to save the input file.
+
+    Raises:
+        RuntimeError: If the request to fetch the input fails.
+
+    Returns:
+        None
+    """
+
     session_id = get_session_id()
 
     url = f"https://adventofcode.com/{year}/day/{day}/input"
@@ -27,7 +50,19 @@ def make_input(year, day, path):
         f.write(response.text[:-1])
 
 
-def make_files(year, day):
+def make_files(year, day) -> None:
+    """
+    Creates the necessary files and directories for the specified year and day
+    of the Advent of Code challenge.
+
+    Args:
+        year (int): The year.
+        day (int): The day.
+
+    Returns:
+        None
+    """
+
     path = f"./{year}/day{day:02d}/"
 
     # make directories
